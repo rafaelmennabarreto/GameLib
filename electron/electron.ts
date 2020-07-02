97d2187c1f576d7a0325c74e7f75b800b080f062
Rafael Barreto <rsmennabarreto@gmail.com>
2020-07-02 18:39:50 -0300

    Initial commit

---
+++ electron/electron.ts
@@ -0,0 +1,34 @@
+import { app, BrowserWindow } from 'electron';
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  console.log(process.env.NODE_ENV);
+
+  if(process.env.NODE_ENV === "development" )
+    win.loadURL('http://localhost:3000');
+  else
+    win.loadFile('index.html')
+
+  win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
